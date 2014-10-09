91724e66180314adcef26b993b84e4b4133bae62
David Van <David@DavidVan.net>
2014-10-09 15:38:18 -0700

    Updated main class.

--- Happy Numbers/src/HappyNumber.java
+++ Happy Numbers/src/HappyNumber.java
@@ -1,4 +1,16 @@
+import java.util.Scanner;
 
 public class HappyNumber {
-
+	public static int userInput;
+	public static CalculateNumber calculate;
+	public static void main(String[] args) {
+		calculate = new CalculateNumber();
+		askForNumber();
+		calculate.number(userInput);
+	}
+	public static void askForNumber() {
+		System.out.println("Please enter a number to check if it's happy or not.");
+		Scanner scan = new Scanner(System.in);
+		userInput = scan.nextInt();
+	}
 }
