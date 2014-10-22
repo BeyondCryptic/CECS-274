c0dded1f99cf94cde09d32605148c9e013954c19
David Van <David@DavidVan.net>
2014-10-21 18:21:24 -0700

    Finished Stack class, I think!

--- RPN Equations/src/Stack.java
+++ RPN Equations/src/Stack.java
@@ -1,6 +1,16 @@
+import java.util.LinkedList;
 
 public class Stack {
+	
+	public LinkedList<String> myList;
+	
 	public Stack() {
-		
+		myList = new LinkedList<String>();
+	}
+	public void push(String myString){
+		myList.addLast(myString);
+	}
+	public String pop() {
+		return myList.removeLast();
 	}
 }
