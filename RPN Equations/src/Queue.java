86eb4671340778cc3e6febfa20db15b1c5a1924d
David Van <David@DavidVan.net>
2014-10-21 18:15:44 -0700

    Finished Queue class, I think!

--- RPN Equations/src/Queue.java
+++ RPN Equations/src/Queue.java
@@ -1,6 +1,16 @@
+import java.util.LinkedList;
 
 public class Queue {
+	
+	public LinkedList<String> myList;
+	
 	public Queue() {
-		
+		myList = new LinkedList<String>();
+	}
+	public void enqueue(String myString) {
+		myList.addLast(myString);
+	}
+	public String dequeue(String myString) {
+		return myList.removeFirst();
 	}
 }
