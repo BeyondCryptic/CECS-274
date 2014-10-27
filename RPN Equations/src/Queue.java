a1e52c7a7b5a311ba7b85cf5e8c622ed4913987c
David Van <David@DavidVan.net>
2014-10-26 22:37:30 -0700

    Added counter to queue class.

--- RPN Equations/src/Queue.java
+++ RPN Equations/src/Queue.java
@@ -1,10 +1,11 @@
 import java.util.LinkedList;
 
 public class Queue {
-	
+	public int counter;
 	public LinkedList<String> myList;
 	
 	public Queue() {
+		counter = 0;
 		myList = new LinkedList<String>();
 	}
 	public void enqueue(String myString) {
@@ -12,5 +13,8 @@ public class Queue {
 	}
 	public String dequeue() {
 		return myList.removeFirst();
+	}
+	public int getCount() {
+		return counter;
 	}
 }
