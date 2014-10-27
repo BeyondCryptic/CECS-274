f477e4c4c7c15727b403fa3b29cced9dee708e74
David Van <David@DavidVan.net>
2014-10-26 22:20:12 -0700

    Added counter to stack class.

--- RPN Equations/src/Stack.java
+++ RPN Equations/src/Stack.java
@@ -1,16 +1,19 @@
 import java.util.LinkedList;
 
 public class Stack {
-	
+	public int counter;
 	public LinkedList<String> myList;
 	
 	public Stack() {
+		counter = 0;
 		myList = new LinkedList<String>();
 	}
 	public void push(String myString){
+		counter++;
 		myList.addLast(myString);
 	}
 	public String pop() {
+		counter--;
 		return myList.removeLast();
 	}
 }
